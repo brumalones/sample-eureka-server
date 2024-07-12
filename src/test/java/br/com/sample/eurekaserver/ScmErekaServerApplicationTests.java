eb51baf5f4c4dce1a9aa83a0c8a1ce7686a7f004
Bruno Matheus <brumalones@gmail.com>
2024-07-12 18:21:27 -0300

    Initial Project.

---
+++ src/test/java/br/com/sample/eurekaserver/ScmErekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sample.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmErekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
